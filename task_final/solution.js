821e3381bee1de0cd42d9f7d1a7852161d24fad3
olgakostina12 <olga.kostina@fogstream.ru>
2021-07-22 00:22:48 +1000

    final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,19 +34,18 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client = '', goods: [title = {}, count = {}], order: {address, sum}};
 
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+        data.goods.title.push(goods[i].title);
+        data.goods.count.push(goods[i].count);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
+    data.order.address = String(address);
+    data.order.sum = sum;
+    data.client = name + ' ' + phone;
     let jsonData = JSON.stringify(data);
 
     return jsonData;
