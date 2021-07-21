718baa7cfaf4716442384da9825b7dfa646cf6e4
olgakostina12 <olga.kostina@fogstream.ru>
2021-07-22 00:14:54 +1000

    task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,13 +2,22 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
-
+    let data = JSON.parse(json);
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-}
+    return data.products
 
+}
+function renderProductsCards(json) {
+    clearProducts();
+    let products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i += 1) {
+        addProduct(products[i])
+    }
+}
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
