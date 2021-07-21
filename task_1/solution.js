cc4dbef81a5c38a331373d23aaafc4e2c6e22b42
olgakostina12 <olga.kostina@fogstream.ru>
2021-07-21 23:56:17 +1000

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,9 +1,17 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+      
 
     // Задание №1.1. Оповещение о добавлении в корзину
-
+    let message;
+    message = productName;
+    message = message + " ";
+    message = message + 'за';
+    message = message + " ";
+    message = message + productPrice;
+    message = message + " ";
+    message = message + "теперь в корзине!";
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
@@ -23,6 +31,9 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue;
+    newValue = oldValue;
+    newValue = newValue + 1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,9 +47,13 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
+  
     // Задание №1.3. Увеличение суммы в корзине
-
+    let newSum = oldSum;
+    newSum = newSum + difference;
+    let newSumText;
+    newSumText = newSum + "₽";
+    newSumText = `${newSum} ₽`;
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
