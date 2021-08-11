1366dece233482b8037b0090588aa26062276277
olgakostina12 <olga.kostina@fogstream.ru>
2021-08-11 22:19:25 +1000

    final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,15 +39,14 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        let obj = {'title': goods[i].title, 'count': goods[i].count};
-        data.goods.push(obj);
+        data.goods.push({'title': goods[i].title, 'count': goods[i].count});
     }
-    let ad = 'ул. ' + address.street + ', ' + 'дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв '  + address.flat;
-    let s = sum;
-    let gd = {'address': ad, 'sum': s};
-    data.order = gd;
+    let adress = 'ул. ' + address.street + ', ' + 'дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв '  + address.flat;
+    data.order = {'address': adress, 'sum': sum};
     data.client = name + ' ' + phone;
 
     let jsonData = JSON.stringify({data: data});
 
     return jsonData;
+}
+
